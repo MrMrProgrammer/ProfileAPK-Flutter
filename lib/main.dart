ca1577fcb5b37dcaca011f23dabffaacd53e97ca
MrMrProgrammer <mr.mr.programmer@gmail.com>
2025-03-09 23:41:53 +0330

    continue project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,9 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 10, 250, 246)),
         useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
     );
@@ -25,9 +25,63 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Profile'),
+          actions: [
+            Icon(CupertinoIcons.chat_bubble),
+            SizedBox(
+              width: 8,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 16, 0),
+              child: Icon(CupertinoIcons.ellipsis_vertical),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        'assets/images/profile_image.png',
+                        width: 60,
+                        height: 60,
+                      )),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('MrMrProgrammer'),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text('Backend Developer'),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Row(
+                        children: [
+                          Icon(CupertinoIcons.location_solid),
+                          Text('Tehran, Iran'),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 0, 32, 16),
+              child: Text(
+                  "My Name is Mohammadreza. I'm 24 years old and I'm Python Developer. I'm currently work on Bijak."),
+            ),
+          ],
+        ));
   }
 }
