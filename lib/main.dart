78e329e14c1cbf993ff32522c46ac52b6dfc1bb5
MrMrProgrammer <mr.mr.programmer@gmail.com>
2025-03-10 11:49:44 +0330

    continue project

--- lib/main.dart
+++ lib/main.dart
@@ -13,35 +13,64 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        useMaterial3: true,
         primarySwatch: Colors.blue,
+        primaryColor: Colors.pink.shade400,
+        dividerColor: Colors.white,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 30, 30, 30),
+        textTheme: const TextTheme(),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+enum _SkillType {
+  photoshop,
+  xd,
+  illustrator,
+  affterEffect,
+  lightRoom
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  _SkillType _skill = _SkillType.photoshop;
+  void updateSelectedSkill(_SkillType skillType) {
+    setState(() {
+      _skill = skillType;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Profile'),
-          actions: [
+          actions: const [
             Icon(CupertinoIcons.chat_bubble),
             SizedBox(
               width: 8,
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(8, 0, 16, 0),
+              padding: EdgeInsets.fromLTRB(8, 0, 16, 0),
               child: Icon(CupertinoIcons.ellipsis_vertical),
             ),
           ],
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.all(32),
+              padding: const EdgeInsets.all(30),
               child: Row(
                 children: [
                   ClipRRect(
@@ -51,37 +80,178 @@ class MyHomePage extends StatelessWidget {
                         width: 60,
                         height: 60,
                       )),
-                  SizedBox(
+                  const SizedBox(
                     width: 16,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('MrMrProgrammer'),
-                      SizedBox(
-                        height: 2,
-                      ),
-                      Text('Backend Developer'),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Row(
-                        children: [
-                          Icon(CupertinoIcons.location_solid),
-                          Text('Tehran, Iran'),
-                        ],
-                      )
-                    ],
-                  )
+                  const Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('MrMrProgrammer'),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text('Backend Developer'),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.location_solid, size: 14,),
+                            Text('Tehran, Iran'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Icon(CupertinoIcons.heart, color: Theme.of(context).primaryColor,),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(32, 0, 32, 16),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(30, 0, 30, 16),
               child: Text(
-                  "My Name is Mohammadreza. I'm 24 years old and I'm Python Developer. I'm currently work on Bijak."),
+                "My Name is Mohammadreza. I'm 24 years old and I'm Python Developer. I'm currently work on Bijak."
+              ),
             ),
+            const Divider(),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(30, 16, 30, 12),
+              child: Row(
+                children: [
+                  Text(
+                    'Skills',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(width: 2,),
+                  Icon(
+                    CupertinoIcons.chevron_down,
+                    size: 10,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Center(
+              child: Wrap(
+                direction: Axis.horizontal,
+                spacing: 8,
+                runSpacing: 8,
+                children: [
+                  Skill(
+                    type: _SkillType.photoshop,
+                    title: 'Photoshop',
+                    imagePath: 'assets/images/app_icon_01.png',
+                    shadowColor: Colors.blue,
+                    isActive: _skill == _SkillType.photoshop,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.photoshop);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.xd,
+                    title: 'Adobe XD',
+                    imagePath: 'assets/images/app_icon_05.png',
+                    shadowColor: Colors.pink,
+                    isActive: _skill == _SkillType.xd,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.xd);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.illustrator,
+                    title: 'Illustrator',
+                    imagePath: 'assets/images/app_icon_04.png',
+                    shadowColor: Colors.orange,
+                    isActive: _skill == _SkillType.illustrator,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.illustrator);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.affterEffect,
+                    title: 'After Effect',
+                    imagePath: 'assets/images/app_icon_03.png',
+                    shadowColor: Colors.blue,
+                    isActive: _skill == _SkillType.affterEffect,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.affterEffect);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.lightRoom,
+                    title: 'Lightroom',
+                    imagePath: 'assets/images/app_icon_02.png',
+                    shadowColor: Colors.blue,
+                    isActive: _skill == _SkillType.lightRoom,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.lightRoom);
+                    },
+                  ),
+
+                ],
+              ),
+            )
           ],
         ));
+  }
+}
+
+class Skill extends StatelessWidget {
+  final _SkillType type;
+  final String title;
+  final String imagePath;
+  final Color shadowColor;
+  final bool isActive;
+  final Function() onTap;
+
+  const Skill({
+    super.key,
+    required this.type,
+    required this.title,
+    required this.imagePath,
+    required this.shadowColor,
+    required this.isActive,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final BorderRadius defaultBorderRadius = BorderRadius.circular(12);
+    return InkWell(
+      borderRadius: defaultBorderRadius,
+      onTap: onTap,
+      child: Container(
+        width: 110,
+        height: 110,
+        decoration: isActive ? BoxDecoration(
+            color: const Color(0x0dffffff),
+            borderRadius: defaultBorderRadius
+        ) : null,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: isActive ? BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: shadowColor,
+                      blurRadius: 20
+                  ),
+                ]) : null,
+              child: Image.asset(
+                imagePath,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            const SizedBox(height: 8,),
+            Text(title),
+          ],
+        ),
+      ),
+    );
   }
 }
